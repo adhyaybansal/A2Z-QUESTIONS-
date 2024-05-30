dcfcd213df84fa1cf81ba5c69ea4f84d3d068b97
ADHYAY BANSAL <142560446+adhyaybansal@users.noreply.github.com>
2024-05-30 22:29:30 +0530

    IF ELSE AND SWTICH

---
+++ q4.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <iostream>;
+#include <vector>;
+#include<algorithm>;
+#include <string>;
+#include <unordered_map> ;
+#include <iomanip> ;
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%lf ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        cout<<setprecision(3)<<fixed;
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i]<< " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+  float areac;
+  float arear;
+    double switchCase(int choice, vector<double> &arr) {
+        switch(choice){
+            case 1:
+                areac = 3.14159*arr[0]*arr[0];
+                return areac;
+                break;
+            case 2:
+                arear = arr[0]*arr[1];
+                return arear;
+                break;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int choice;
+        scanf("%d",&choice);
+        
+        
+        vector<double> arr(choice);
+        Array::input(arr,choice);
+        
+        Solution obj;
+        double res = obj.switchCase(choice, arr);
+        // cout<<fixed<<endl;
+        if(res == int(res)){
+            cout<<int(res)<<endl;
+            continue;
+        }
+        cout<<setprecision(2)<<fixed;
+        // 
+        // }else{
+            cout<<res<<endl;
+        // }
+        
+    }
+}
+
+// } Driver Code Ends
