59ce4f447ed337274598a79e6d10633feaab64d9
ADHYAY BANSAL <142560446+adhyaybansal@users.noreply.github.com>
2024-05-30 23:00:23 +0530

    NTH FIBONACCI

--- q5.cpp
+++ q5.cpp
@@ -5,26 +5,34 @@ using namespace std;
 class Fibonacci{
     public:
     vector<int> arr;
-    int n=10000;
+    
     void Fibonaccin(int n){
         if(n==0){
             cout<< 0<<endl;
         }
+        else if(n==1){
+            cout<< 1<<endl;
+        }
         else{
-            arr[0] =1;
-            for(int i=1;i<=n;i++){
+            arr.push_back(0);
+            arr.push_back(1);
+            for(int i=2;i<=n;i++){
                 arr.push_back({arr[i-1]+arr[i-2]});
             }
         }
-        for(int i=0;i<=n;i++){
-            cout<<arr[i]<<" ";
     }
+    void getout(int in){
+        cout<<arr[in]<<endl;
     }
 
 };
 int main(){
     Fibonacci obj;
-    obj.Fibonaccin(10);
+    int input;
+    cin>>input;
+    int n=10000;
+    obj.Fibonaccin(n);
+    obj.getout(input);
     
 
     return 0;
