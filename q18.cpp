11c04d6cef5669f45730a0db9588a17eed9a9e69
ADHYAY BANSAL <142560446+adhyaybansal@users.noreply.github.com>
2024-06-04 20:13:38 +0530

    Create q18.cpp

---
+++ q18.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int cnt=1;
+    void printNos(int N)
+    {
+        if(cnt==N+1) return ;
+        cout<<cnt<<" ";
+        
+        cnt++;
+        printNos(N);
+    }
+    
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    
+    while(T--)
+    {
+        int N;
+        
+        //input N
+        cin>>N;
+        Solution ob;
+        //calling printNos() function
+        ob.printNos(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
