915dde1d3b4ce7cc59db03195680ed5a1156e6a6
ADHYAY BANSAL <142560446+adhyaybansal@users.noreply.github.com>
2024-06-09 20:22:33 +0530

    Update q9.cpp

--- q9.cpp
+++ q9.cpp
@@ -8,7 +8,7 @@ class Solution {
   public:
     void printTriangle(int n) {
         for(int i=0;i<=n;i++){
-            for(int j=0;j<=i-1;j++){
+            for(int j=0;j<=i;j++){
                 cout<<"*"<<" ";
             }
             cout<<endl;
